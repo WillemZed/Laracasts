1d6b73a057cc6cc20eea1f1f705846c7ca4534e8
Willem Xu <willem.zheng.xu@gmail.com>
2022-02-07 14:47:54 +0100

    26 - Clockwork N+1 problems, clockwork werkt nog niet

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
@@ -17,19 +19,28 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
+    DB::listen(function ($query) {
+        logger($query->sql, $query->bindings);
+    });
+
     return view('posts', [
         "posts" => Post::all()
     ]);
 });
 
 
-Route::get('posts/{post}', function (Post $post) {
+Route::get('posts/{post:slug}', function (Post $post) {
     // Find a post by slug and pass it to view "post"
 
     return view("post", [
         "post" => $post
     ]);
+});
 
+Route::get('categories/{category:slug}', function(Category $category) {
+    return view('posts', [
+        "posts" => $category->posts
+    ]);
 });
 
 
